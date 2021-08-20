9b4448444a09773a470d45e56a53542d5d088798
hataevicha5922 <hataevicha5922@gmail.com>
2021-08-20 07:07:43 +0300

    add expenses

---
+++ src/components/expenses-handler/expenses-handler.js
@@ -0,0 +1,60 @@
+import moment from 'moment';
+import { getExpenses } from '../../api/api-handlers';
+import { LocalStorageService } from '../../shared/ls-service';
+
+const userNameTag = document.getElementById('header-links-info');
+
+export let expensesResult;
+export const renderExpenses = async () => {
+  const expensesContainer = document.getElementById('expenses-container');
+  const userId = LocalStorageService.getPersonalData().id;
+  const expensesSum = document.getElementById('expenses-sum');
+
+  let expenses;
+
+  expensesContainer.innerHTML = null;
+
+  userNameTag.innerText = `${LocalStorageService.getPersonalData().firstName} ${
+    LocalStorageService.getPersonalData().lastName
+  }`;
+
+  await getExpenses().then((result) => (expenses = result.data));
+
+  let expensesArr = [];
+  Object.values(expenses).map((expense) =>
+    expense.userId === userId ? expensesArr.push(expense) : false
+  );
+
+  expensesArr.forEach((expense) => {
+    const incomeWrapper = document.createElement('div');
+    const incomeBlock = document.createElement('div');
+    const title = document.createElement('h5');
+    const value = document.createElement('p');
+    const userName = document.createElement('span');
+    const incomeDate = document.createElement('span');
+    const buttonWrapper = document.createElement('div');
+    const buttonChange = document.createElement('button');
+    const buttonDelete = document.createElement('button');
+
+    incomeWrapper.className = 'income-wrapper';
+    incomeBlock.className = 'income-info';
+    userName.className = 'income-user-name';
+    value.className = 'income-value';
+    buttonWrapper.className = 'income-btns';
+    buttonChange.className = 'change-button';
+    buttonDelete.className = 'delete-button';
+
+    title.innerHTML = expense.categoriesExpenses;
+    value.innerHTML = `${expense.valueExpenses} BYN`;
+    incomeDate.innerHTML = moment(expense.date).format('MMM Do YY');
+    incomeBlock.append(title, value, userName, incomeDate);
+    buttonWrapper.append(buttonChange, buttonDelete);
+    incomeWrapper.append(incomeBlock, buttonWrapper);
+    expensesContainer.append(incomeWrapper);
+  });
+
+  expensesResult = expensesArr.reduce((res, expense) => {
+    return res + +expense.valueExpenses;
+  }, 0);
+  expensesSum.innerText = `Expenses: ${expensesResult} BYN`;
+};
--- src/components/expenses/expenses.js
+++ src/components/expenses/expenses.js
@@ -1,25 +1,35 @@
-import { setExpensesRes } from '../../api/api-handlers';
+import moment from 'moment';
+import { setExpenses } from '../../api/api-handlers';
 import { routes } from '../../shared/constants/routs';
+import { LocalStorageService } from '../../shared/ls-service';
 
 export const expensesHandler = () => {
   const expensesForm = document.getElementById('expenses-form');
   const expensesInputValue = document.getElementById('expenses-input');
   const expenseCurrency = document.getElementById('expenses-currency');
   const expensesCategories = document.getElementById('expenses-categories');
+  const userName = document.getElementById('header-links-info');
+
+  userName.innerText = userName.innerText = `${
+    LocalStorageService.getPersonalData().firstName
+  } ${LocalStorageService.getPersonalData().lastName}`;
+
+  const expenses = {
+    valueExpenses: null,
+    categoriesExpenses: null,
+    userId: LocalStorageService.getPersonalData().id,
+    date: moment().format(),
+  };
 
   expensesForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const expenses = {
-      valueExpenses: expensesInputValue.value,
-      categoriesExpenses: expensesCategories.value,
-      currencyExpenses: expenseCurrency.value,
-    };
-
-    setExpensesRes(expenses).then((response) => {
-      if (response) {
-        window.location.href = routes.main_page;
-      }
-    });
+    (expenses.valueExpenses = expensesInputValue.value),
+      (expenses.categoriesExpenses = expensesCategories.value),
+      setExpenses(expenses).then((response) => {
+        if (response) {
+          window.location.href = routes.main_page;
+        }
+      });
   });
 };
